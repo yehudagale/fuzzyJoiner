19322bd526f4aa868e980086fd4449c9da3b1b7e
Kavitha Srinivas <kavitha@rivetlabs.io>
2017-11-08 09:19:24 -0500

    test.py fixed

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 from embeddings import KazumaCharEmbedding
 k = KazumaCharEmbedding()
+
+k = KazumaCharEmbedding()
+for w in ['canada', 'vancouver', 'toronto', 'BMT3325']:
+    print('embedding {}'.format(w))
+    print(k.emb(w))
