e8a08fedc22ffdf279e4b64f05cf7755ef2c6d81
Kavitha Srinivas <kavitha@rivetlabs.io>
2018-05-27 22:33:01 -0400

    interim state for different experiments - code is not thoroughly tested

--- TripletLossFacenetLSTM.py
+++ TripletLossFacenetLSTM.py
@@ -30,18 +30,25 @@ import sys
 
 import statistics 
 
+import argparse
+
 #must fix
 MAX_NB_WORDS = 140000
 EMBEDDING_DIM = 100
 MAX_SEQUENCE_LENGTH = 10
-MARGIN=1
+MARGIN=30
 ALPHA=30
 
-DEBUG = False
+DEBUG = True
 DEBUG_DATA_LENGTH = 1000
 DEBUG_ANN = False
 
 USE_ANGULAR_LOSS=False
+LOSS_FUNCTION=None
+TRAIN_NEIGHBOR_LEN=20
+TEST_NEIGHBOR_LEN=20
+EMBEDDING_TYPE = 'Kazuma'
+NUM_LAYERS = 3
 
 
 def f1score(positive, negative):
@@ -113,13 +120,10 @@ def get_sequences(texts, tokenizer):
 
 def read_entities(filepath):
     entities = []
-    i = 0
     with open(filepath) as fl:
         for line in fl:
             entities.append(line)
-            i += 1
-            if i > DEBUG_DATA_LENGTH and DEBUG:
-                break
+
     return entities
 
 def read_file(file_path):
@@ -138,8 +142,12 @@ def read_file(file_path):
 
 def split(entities, test_split = 0.2):
     random.shuffle(entities)
-    num_validation_samples = int(test_split * len(entities))
-    return entities[:-num_validation_samples], entities[-num_validation_samples:]
+    if DEBUG:
+        ents = entities[0:DEBUG_DATA_LENGTH]
+    else:
+        ents = entities
+    num_validation_samples = int(test_split * len(ents))
+    return ents[:-num_validation_samples], ents[-num_validation_samples:]
 
 """
   define a single objective function based on angular loss instead of triplet loss
@@ -221,7 +229,7 @@ def build_unique_entities(entity2same):
     return unique_text, entity2index
 
 
-def generate_triplets_from_ANN(model, sequences, entity2unique, entity2same, unique_text, generate_triplets=True, compute_stats=True):
+def generate_triplets_from_ANN(model, sequences, entity2unique, entity2same, unique_text, test):
     predictions = model.predict(sequences)
     t = AnnoyIndex(len(predictions[0]), metric='euclidean')  # Length of item vector that will be indexed
     for i in range(len(predictions)):
@@ -245,7 +253,11 @@ def generate_triplets_from_ANN(model, sequences, entity2unique, entity2same, uni
     triplets['positive'] = []
     triplets['negative'] = []
 
-    NNlen = 20
+    if test:
+        NNlen = TEST_NEIGHBOR_LEN
+    else:
+        NNlen = TRAIN_NEIGHBOR_LEN
+
     for key in entity2same:
         index = entity2unique[key]
         nearest = t.get_nns_by_vector(predictions[index], NNlen)
@@ -259,7 +271,7 @@ def generate_triplets_from_ANN(model, sequences, entity2unique, entity2same, uni
         # collect up some statistics on how well we did on the match
         m = len(overlap)
         match += m
-        # since we asked for only 10 nearest neighbors, and we get at most 9 neighbors that are not the same as key (!)
+        # since we asked for only x nearest neighbors, and we get at most x-1 neighbors that are not the same as key (!)
         # make sure we adjust our estimate of no match appropriately
         no_match += min(len(expected_text), NNlen - 1) - m
 
@@ -286,19 +298,18 @@ def generate_triplets_from_ANN(model, sequences, entity2unique, entity2same, uni
                 triplets['positive'].append(j)
                 triplets['negative'].append(i)
 
-    if compute_stats:
-        print("mean positive distance:" + str(statistics.mean(pos_distances)))
-        print("stdev positive distance:" + str(statistics.stdev(pos_distances)))
-        print("max positive distance:" + str(max(pos_distances)))
-        print("mean neg distance:" + str(statistics.mean(neg_distances)))
-        print("stdev neg distance:" + str(statistics.stdev(neg_distances)))
-        print("max neg distance:" + str(max(neg_distances)))
+    print("mean positive distance:" + str(statistics.mean(pos_distances)))
+    print("stdev positive distance:" + str(statistics.stdev(pos_distances)))
+    print("max positive distance:" + str(max(pos_distances)))
+    print("mean neg distance:" + str(statistics.mean(neg_distances)))
+    print("stdev neg distance:" + str(statistics.stdev(neg_distances)))
+    print("max neg distance:" + str(max(neg_distances)))
+    print("Accuracy in the ANN for triplets that obey the distance func:" + str(accuracy / total))
 
-    if not generate_triplets:
-        print("Accuracy in the ANN for triplets that obey the distance func:" + str(accuracy / total))
+    if test:
+        return match/(match + no_match)
     else:
         return triplets, match/(match + no_match)
-    return match/(match + no_match)
 
 def generate_names(entities, limit_pairs=False):
     num_names = 4
@@ -316,17 +327,30 @@ def embedded_representation_model(embedding_layer):
     seq.add(Flatten())
     return seq
 
+def get_hidden_layer(name, net, is_last):
+    if USE_GRU:
+        if is_last:
+            return GRU(128, activation='relu', name=name)(net)
+        else:
+            return GRU(128, return_sequences=True, activation='relu', name=name)(net)
+    else:
+        return Dense(128, activation='relu', name=name)(net)
+
 def build_model(embedder):
     main_input = Input(shape=(MAX_SEQUENCE_LENGTH,))
     net = embedder(main_input)
-    net = GRU(128, return_sequences=True, activation='relu', name='embed')(net)
-    net = GRU(128, return_sequences=True, activation='relu', name='embed2')(net)
-    net = GRU(128, return_sequences=True, activation='relu', name='embed2a')(net)
-    net = GRU(128, activation='relu', name='embed3')(net)
-    # net = Lambda(l2Norm, output_shape=[128])(net)
-    # net = Lambda(tanhNorm, output_shape=[128])(net)
+
+    for i in range(0, NUM_LAYERS):
+        net = get_hidden_layer('embed' + str(i), net, False)
+
+    net = get_hidden_layer('embed_last', net, True)
+    
+    if USE_L2_NORM:
+        net = Lambda(l2Norm, output_shape=[128])(net)
 
     base_model = Model(embedder.input, net, name='triplet_model')
+
+    base_model.summary()
 
     input_shape=(MAX_SEQUENCE_LENGTH,)
     input_anchor = Input(shape=input_shape, name='input_anchor')
@@ -355,15 +379,75 @@ def build_model(embedder):
                     )([positive_dist, negative_dist, exemplar_negative_dist])
 
         model = Model([input_anchor, input_positive, input_negative], stacked_dists, name='triple_siamese')
-        model.compile(optimizer="rmsprop", loss=triplet_loss, metrics=[accuracy])
+        model.compile(optimizer="rmsprop", loss=LOSS_FUNCTION, metrics=[accuracy])
     test_positive_model = Model([input_anchor, input_positive, input_negative], positive_dist)
     test_negative_model = Model([input_anchor, input_positive, input_negative], negative_dist)
     inter_model = Model(input_anchor, net_anchor)
 
     return model, test_positive_model, test_negative_model, inter_model
 
+
+parser = argparse.ArgumentParser(description='Run fuzzy join algorithm')
+parser.add_argument('--debug_sample_size', type=int,
+                    help='sample size for debug run')
+parser.add_argument('--margin',  type=int,
+                    help='margin')
+parser.add_argument('--loss_function',  type=str,
+                    help='loss function type: triplet-loss, improved-triplet-loss, modified-loss, or angular-loss')
+parser.add_argument('--use_l2_norm',  type=bool,
+                    help='whether to add a l2 norm')
+parser.add_argument('--test_neighbor_len',  type=int,
+                    help='size of the neighborhood for testing')
+parser.add_argument('--train_neighbor_len',  type=int,
+                   help='size of the neighborhood for training')
+parser.add_argument('--embedding_type', type=str, help='encoding type to use for input: Kazuma (for Kazuma character embedding) or one-hot')
+parser.add_argument('--use_GRU', type=bool,
+                   help='use GRU or default to MLP')
+parser.add_argument('--num_layers', type=int,
+                   help='num_layers to use.  Minimum is 2')
+parser.add_argument('--input', type=str, help='Input file')
+
+
+args = parser.parse_args()
+if args.debug_sample_size:
+    DEBUG=True
+    DEBUG_DATA_LENGTH=args.debug_sample_size
+    print('Debug data length:' + str(DEBUG_DATA_LENGTH))
+
+MARGIN = args.margin
+print('Margin:' + str(MARGIN))
+
+LOSS_FUNCTION = None
+if args.loss_function == 'triplet-loss':
+    LOSS_FUNCTION=schroff_triplet_loss
+elif args.loss_function == 'improved_triplet_loss':
+    LOSS_FUNCTION=improved_triplet_loss
+elif args.loss_function == 'modified_loss':
+    LOSS_FUNCTION=triplet_loss
+elif args.loss_function == 'angular-loss':
+    USE_ANGULAR_LOSS = true
+    LOSS_FUNCTION = angular_loss
+print('Loss function: ' + args.loss_function)
+
+TRAIN_NEIGHBOR_LEN = args.train_neighbor_len
+TEST_NEIGHBOR_LEN = args.test_neighbor_len
+print('Train neighbor length: ' + str(TRAIN_NEIGHBOR_LEN))
+print('Test neighbor length: ' + str(TEST_NEIGHBOR_LEN))
+
+USE_L2_NORM = args.use_l2_norm
+print('Use L2Norm: ' + str(USE_L2_NORM)) 
+
+EMBEDDING_TYPE = args.embedding_type
+print('Embedding type: ' + EMBEDDING_TYPE)
+
+USE_GRU = args.use_GRU
+print('Use GRU: ' + str(args.use_GRU))
+
+NUM_LAYERS = args.num_layers - 1
+print('Num layers: ' + str(NUM_LAYERS))
+
 # read all entities and create positive parts of a triplet
-entities = read_entities(argv[1])
+entities = read_entities(args.input)
 train, test = split(entities, test_split = .20)
 
 entity2same_train = generate_names(train)
@@ -395,10 +479,10 @@ embedder_model = embedded_representation_model(embedder)
 
 
 if DEBUG_ANN:
-    generate_triplets_from_ANN(embedder_model, sequences_test, entity2unique_test, entity2same_test, unique_text_test, False, True)
+    generate_triplets_from_ANN(embedder_model, sequences_test, entity2unique_test, entity2same_test, unique_text_test, True)
     sys.exit()
 
-test_data, test_match_stats = generate_triplets_from_ANN(embedder_model, sequences_test, entity2unique_test, entity2same_test, unique_text_test)
+test_data, test_match_stats = generate_triplets_from_ANN(embedder_model, sequences_test, entity2unique_test, entity2same_test, unique_text_test, False)
 test_seq = get_sequences(test_data, tokenizer)
 print("Test stats:" + str(test_match_stats))
 
@@ -411,7 +495,7 @@ prev_match_stats = 0
 
 while test_match_stats < .9 and counter < num_iter:
     counter += 1
-    train_data, match_stats = generate_triplets_from_ANN(current_model, sequences, entity2unique, entity2same_train, unique_text)
+    train_data, match_stats = generate_triplets_from_ANN(current_model, sequences, entity2unique, entity2same_train, unique_text, False)
     print("Match stats:" + str(match_stats))
  
     number_of_names = len(train_data['anchor'])
@@ -432,6 +516,7 @@ while test_match_stats < .9 and counter < num_iter:
     model.fit([train_seq['anchor'], train_seq['positive'], train_seq['negative']], Y_train, epochs=1,  batch_size=40, callbacks=callbacks_list, validation_split=0.2)
     current_model = inter_model
     # print some statistics on this epoch
+    
     print("training data predictions")
     positives = test_positive_model.predict([train_seq['anchor'], train_seq['positive'], train_seq['negative']])
     negatives = test_negative_model.predict([train_seq['anchor'], train_seq['positive'], train_seq['negative']])
@@ -440,8 +525,9 @@ while test_match_stats < .9 and counter < num_iter:
     positives = test_positive_model.predict([test_seq['anchor'], test_seq['positive'], test_seq['negative']])
     negatives = test_negative_model.predict([test_seq['anchor'], test_seq['positive'], test_seq['negative']])
     print("f1score for test is: {}".format(f1score(positives, negatives)))
+    
 
-    test_match_stats = generate_triplets_from_ANN(current_model, sequences_test, entity2unique_test, entity2same_test, unique_text_test, False)
+    test_match_stats = generate_triplets_from_ANN(current_model, sequences_test, entity2unique_test, entity2same_test, unique_text_test, True)
     print("Test stats:" + str(test_match_stats))
 
 
